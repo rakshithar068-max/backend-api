7e8c228b975962689cb439e1652fae92c8112525
rakshithar068-max <rakshithar068@gmail.com>
2026-05-12 14:35:50 +0530

    pipeline change for ACR

--- Program.cs
+++ Program.cs
@@ -15,7 +15,11 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("LocalDevCors", policy =>
     {
-        policy.WithOrigins("https://react-rakshitha-czhwfef3hce2fjaf.southindia-01.azurewebsites.net","http://localhost:3000")
+        policy.WithOrigins(
+                "https://react-rakshitha-czhwfef3hce2fjaf.southindia-01.azurewebsites.net",
+                "http://localhost:3000",
+                "http://rakshitha-react-app-123.southindia.azurecontainer.io"
+              )
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
